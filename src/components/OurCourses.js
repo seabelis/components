aaae4beb6760ed7b577e24a272898948ea0a5ade
seabelis <seabelis74@gmail.com>
2020-10-29 17:15:09 +0100

    add alt

--- src/components/OurCourses.js
+++ src/components/OurCourses.js
@@ -4,10 +4,10 @@ export default class OurCourses extends Component {
   render() {
     return <header id="header" className="section-header scroll">
     <a href="./">
-      <img className="icon" src="https://learntocodetogether.nl/assets/icon.svg" />
+      <img className="icon" alt="learn to code icon" src="https://learntocodetogether.nl/assets/icon.svg" />
     </a>
     <a href="https://www.meetup.com/Learning-to-Code-Amsterdam/">
-      <img className="meetup-icon" src="https://learntocodetogether.nl/assets/meetup-icon.svg" />
+      <img className="meetup-icon" alt="meetup icon" src="https://learntocodetogether.nl/assets/meetup-icon.svg" />
     </a>
     <nav>
       <a href="/courses.html">Our Courses</a>
